16d142e76ded8de142bd4baa3266b28bbd8db74e
jdsi23 <jameslusk535@gmail.com>
2025-04-21 20:37:21 +0000

    Initial commit of Terraform secure storage config

--- main.tf
+++ main.tf
@@ -1,33 +1,31 @@
 terraform {
   required_version = ">=1.3.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">3.43.0"
+      version = ">=3.43.0"
     }
-  }
-}
-terraform {
-  required_providers {
     hcp = {
-      source = "hashicorp/hcp"
+      source  = "hashicorp/hcp"
       version = "0.104.0"
     }
   }
 }
 
-provider "hcp" {
-  # Configuration options
-}
 provider "azurerm" {
   features {}
 }
 
+provider "hcp" {
+  # Configuration options
+}
+
 resource "azurerm_storage_account" "securestorage" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
+  name                         = var.storage_account_name
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  account_tier                 = var.account_tier
+  account_replication_type     = var.account_replication_type
   public_network_access_enabled = false
 }
